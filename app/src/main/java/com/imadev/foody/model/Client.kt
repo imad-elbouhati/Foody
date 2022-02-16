ef719b1669efe952cca1add45206f35706c8f9ab
Doha <dohalefhal4@gmail.com>
2022-02-16 22:38:16 +0100

    change adress

--- app/src/main/java/com/imadev/foody/model/Client.kt
+++ app/src/main/java/com/imadev/foody/model/Client.kt
@@ -2,6 +2,6 @@ package com.imadev.foody.model
 
 class Client(
     val username: String = "Doha lefhal",
-    val address: String = "Rue jrada hay amal",
+    val address: String = "Rue jrada hay amal karia salé",
     val phone: String = "+212 623267604"
 )
