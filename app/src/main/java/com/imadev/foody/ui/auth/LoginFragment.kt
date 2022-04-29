7af6070c53b8d8a1519dfbf8cc9c541f0bfe8b35
doha <dohalefhal4@gmail.com>
2022-04-29 00:56:59 -0230

    sign_in_facebook

--- app/src/main/java/com/imadev/foody/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/imadev/foody/ui/auth/LoginFragment.kt
@@ -9,11 +9,19 @@ import android.view.LayoutInflater
 import android.view.View
 
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
@@ -29,12 +37,15 @@ import com.imadev.foody.utils.hide
 private const val TAG = "LoginFragment"
 
 class LoginFragment : BaseFragment<FragmentLoginBinding, AuthViewModel>() {
+
     override val viewModel: AuthViewModel by viewModels()
 
     // Configure Google Sign In
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
-    
+
+    private var callbackManager: CallbackManager? = null
+
     override fun createViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -45,6 +56,13 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, AuthViewModel>() {
         super.onViewCreated(view, savedInstanceState)
 
         auth = Firebase.auth
+
+        callbackManager = CallbackManager.Factory.create();
+
+binding.facebookSignInBtn.setOnClickListener {
+signInWithFacebook()
+}
+
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -61,16 +79,61 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, AuthViewModel>() {
         }
     }
 
+    private fun signInWithFacebook() {
+        LoginManager.getInstance().logInWithReadPermissions(this, listOf("email"));
+        callbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult> {
+
+            override fun onCancel() {
+                TODO("Not yet implemented")
+            }
+
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "onError: ${error.message}")
+            }
+
+
+            override fun onSuccess(result: LoginResult) {
+                Log.d(TAG, "onSuccess: ${result?.accessToken?.token.toString()}")
+                handleFacebookAccessToken(result.accessToken)
+            }
+        })
+    }
+
 
     private fun signIn() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
 
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential).addOnCompleteListener{task ->
+
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(requireContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+        }
+
+
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
@@ -99,7 +162,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, AuthViewModel>() {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                 }
-                
+
             }
     }
 
